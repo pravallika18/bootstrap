ad937db2b0e4b062135060c74978723e0c7aafd9
pravallika18 <ppravallika1994@gmail.com>
2017-08-12 01:44:02 +0530

    initial commit

---
+++ includes/js/script.js
@@ -0,0 +1,22 @@
+/*
+
+My Custom JS
+============
+
+Author:  Brad Hussey
+Updated: August 2013
+Notes:	 Hand coded for Udemy.com
+
+*/
+$(function()
+{
+	$('a.pop').click(function(e){
+		e.preventDefault();
+		
+	});
+	$('a.pop').popover();
+	$('#carousel').carousel({
+                interval: 5000
+        });
+
+});
